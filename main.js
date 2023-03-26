6f60ebd9e9c9fed8869ec44d0bfb193110f927b0
ahmedelashkarr <128980260+ahmedelashkarr@users.noreply.github.com>
2023-03-26 17:39:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,14 +4,3 @@ navi.addEventListener("click", function () {
     myli.classList.toggle("show")
 })
 
-// let mybox = document.querySelector("section .container")
-// let myh2 = document.querySelector("section .container .box h2")
-// console.log(mybox)
-// mybox.forEach(ele => {
-//     ele.classList.toggle("color")
-// });
-
-// .addEventListener("mouseover", function () {
-//     this.style.color = "red"
-// })
-// pointerenter
