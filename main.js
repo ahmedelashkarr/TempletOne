28ca9dfec06378a4555ddb9fbdc4abd97c9e160c
Ahmed Elashkar <ahmedelashkar243@gmail.com>
2023-03-26 17:14:20 +0200

    Create page

---
+++ main.js
@@ -0,0 +1,17 @@
+let navi = document.querySelector(".bars")
+let myli = document.querySelector("ul")
+navi.addEventListener("click", function () {
+    myli.classList.toggle("show")
+})
+
+// let mybox = document.querySelector("section .container")
+// let myh2 = document.querySelector("section .container .box h2")
+// console.log(mybox)
+// mybox.forEach(ele => {
+//     ele.classList.toggle("color")
+// });
+
+// .addEventListener("mouseover", function () {
+//     this.style.color = "red"
+// })
+// pointerenter
